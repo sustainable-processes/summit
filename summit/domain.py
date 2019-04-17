b947e9c2448fb35f8933e6a9cb8bff0dc981e6a1
marcosfelt <kcfelton@ncsu.edu>
2019-04-17 06:43:58 +0100

    Update domain to include json serialization

--- summit/domain.py
+++ summit/domain.py
@@ -1,8 +1,9 @@
 from summit.data import DataSet
 import numpy as np
 import pandas as pd
-from typing import List, Optional, Type
+from typing import List, Optional, Type, Dict
 from abc import ABC, abstractmethod
+import json
 
 class Variable(ABC):
     """A base class for variables
@@ -13,17 +14,23 @@ class Variable(ABC):
         The name of the variable
     description: str
         A short description of the variable
+    is_output: bool, optional
+        If True, this variable is an output. Defaults to False (i.e., an input variable)
+    units: str, optional
+        Units of the variable. Defaults to None.
 
     Attributes
     ---------
     name
     description
     """
-    def __init__(self, name: str, description: str, variable_type: str):
+    def __init__(self, name: str, description: str, variable_type: str, **kwargs):
         Variable._check_name(name)
         self._name = name
         self._description = description
         self._variable_type = variable_type
+        self._is_output = kwargs.get('is_output', False)
+        self._units = kwargs.get('units', None)
 
     @property
     def name(self)-> str:
@@ -47,7 +54,28 @@ class Variable(ABC):
     @property
     def variable_type(self) -> str:
         return self._variable_type
+
+    @property
+    def is_output(self) -> bool:
+        return self._is_output
+
+    @property
+    def units(self) -> str:
+        return self._units
+
+    def to_dict(self):
+        variable_dict = {
+           'type': self._variable_type,
+           'is_output': self._is_output,
+           'name': self.name,
+           'description': self.description,
+           'units': self.units}
+        return variable_dict
     
+    def to_json(self):
+        """ Return json encoding of the variable"""
+        return json.dumps(self.to_dict())
+
     @staticmethod
     def _check_name(name: str):
         if type(name) != str:
@@ -66,7 +94,8 @@ class Variable(ABC):
 
     def _make_html_table_rows(self, value):
         name_column = f"<td>{self.name}</td>"
-        type_column = f"<td>{self.variable_type}</td>"
+        input_output = 'output' if self.is_output else 'input'
+        type_column = f"<td>{self.variable_type}, {input_output}</td>"
         description_column = f"<td>{self.description}</td>"
         values_column = f"<td>{value}</td>"
         return f"<tr>{name_column}{type_column}{description_column}{values_column}</tr>"
@@ -82,6 +111,8 @@ class ContinuousVariable(Variable):
         A short description of the variable 
     bounds: list of float or int
         The lower and upper bounds (respectively) of the variable
+    is_output: bool, optional
+        If True, this variable is an output. Defaults to False (i.e., an input variable)
 
     Attributes
     ---------
@@ -97,8 +128,8 @@ class ContinuousVariable(Variable):
 
     """
 
-    def __init__(self, name: str, description: str, bounds: list):
-        Variable.__init__(self, name, description, 'continuous')
+    def __init__(self, name: str, description: str, bounds: list, **kwargs):
+        Variable.__init__(self, name, description, 'continuous', **kwargs)
         self._lower_bound = bounds[0]
         self._upper_bound = bounds[1]
 
@@ -119,6 +150,11 @@ class ContinuousVariable(Variable):
 
     def _html_table_rows(self):
         return self._make_html_table_rows(f"[{self.lower_bound},{self.upper_bound}]")
+    
+    def to_dict(self):
+        variable_dict = super().to_dict()
+        variable_dict.update({'bounds': [float(self.lower_bound), float(self.upper_bound)]})
+        return variable_dict
 
     
 class DiscreteVariable(Variable):
@@ -132,6 +168,8 @@ class DiscreteVariable(Variable):
         A short description of the variable 
     levels: list of any serializable object
         The potential values of the discrete variable
+    is_output: bool, optional
+        If True, this variable is an output. Defaults to False (i.e., an input variable)
 
     Attributes
     ---------
@@ -144,8 +182,8 @@ class DiscreteVariable(Variable):
     >>> reactant = DiscreteVariable('reactant', 'aromatic reactant', ['benzene', 'toluene'])
 
     """
-    def __init__(self, name, description, levels):
-        Variable.__init__(self, name, description, 'discrete')
+    def __init__(self, name, description, levels, **kwargs):
+        Variable.__init__(self, name, description, 'discrete', **kwargs)
         
         #check that levels are unique
         if len(list({v for v in levels})) != len(levels):
@@ -196,6 +234,12 @@ class DiscreteVariable(Variable):
             remove_index = self._levels.index(level)
         except ValueError:
             raise ValueError(f"Level {level} is not in the list of levels.")
+    
+    def to_dict(self):
+        """ Return json encoding of the variable"""
+        variable_dict = super().to_dict()
+        variable_dict.update({'levels': self.levels.tolist()})
+        return variable_dict
 
     def _html_table_rows(self):
         return self._make_html_table_rows(f"{self.num_levels} levels")
@@ -211,6 +255,8 @@ class DescriptorsVariable(Variable):
         A short description of the variable 
     ds: DataSet
         A dataset object
+    is_output: bool, optional
+        If True, this variable is an output. Defaults to False (i.e., an input variable)
 
     Attributes
     ---------
@@ -232,8 +278,8 @@ class DescriptorsVariable(Variable):
     >>> solvent = DescriptorsVariable('solvent', 'solvent descriptors', solvent_df)
     """    
     def __init__(self, name: str, description: str, 
-                 ds: DataSet):
-        Variable.__init__(self, name, description, 'descriptors')
+                 ds: DataSet, **kwargs):
+        Variable.__init__(self, name, description, 'descriptors', **kwargs)
         self.ds = ds
 
     @property
@@ -243,58 +289,21 @@ class DescriptorsVariable(Variable):
     @property
     def num_examples(self):
         return self.ds.shape[0]
+
+    def to_dict(self):
+        """ Return json encoding of the variable"""
+        variable_dict = super().to_dict()
+        variable_dict.update({'ds': self.ds.to_json()})
+        return variable_dict
     
-    # def get_subset(self, select_subset=None, select_index=None, zero_to_one=False):
-    #     ''' Get a subset of the examples of the descriptor 
-        
-    #     Parameters
-    #     ---------- 
-    #     select_subset: list or 1d numpy array, optional
-    #         A list of index keys in the dataframe to select. 
-    #         By default, the select_subset variable from the instance of the class is used.
-    #     select_index: str, optional
-    #         For use with multindex dataframes. This parameter specifies the name of the index to select from.
-    #         By default, the select_index from the instance of the class is used
-    #     zero_to_one: bool, optional
-    #         If true, the descriptors will be scaled to be between 0 and 1. By default, False.
-        
-    #     Returns
-    #     -------
-    #     result: `bool`
-    #         description
-        
-    #     Raises
-    #     ------
-    #     ValueError
-    #         If a select_subset cannot be found        
-    
-    #     '''
-    #     if select_subset is not None:
-    #         pass
-    #     elif select_subset is None and self.select_subset is not None:
-    #         select_subset = self.select_subset
-    #     elif self.select_subset is None:
-    #         raise ValueError("Cannot get subset because select_subset is None")
-
-    #     if not select_index and self.select_index:
-    #         select_index = self.select_index
-
-    #     if zero_to_one:
-    #         df = self.zero_to_one()
-    #     else:
-    #         df = self.df
-
-    #     if select_index:
-    #         for i, index in enumerate(select_subset):
-    #             select = df.xs(index, level=select_index, drop_level=False)
-    #             if i == 0:
-    #                 subset_df = select
-    #             else:
-    #                 subset_df = pd.concat([subset_df, select])
-    #     else:
-    #         subset_df = df.loc[select_subset, :]
-
-    #     return subset_df
+    @staticmethod
+    def from_json(json_str):
+        variable_dict = json.loads(json_str)
+        ds = pd.read_json(variable_dict['ds'])
+        return DescriptorsVariable(name=variable_dict['name'],
+                                   description=variable_dict['description'],
+                                   ds=ds,
+                                   is_output=variable_dict['is_output'])
 
     def _html_table_rows(self):
         return self._make_html_table_rows(f"{self.num_examples} examples of {self.num_descriptors} descriptors")
@@ -347,6 +356,30 @@ class Domain:
             if v.variable_type == 'descriptors':
                 k+= v.num_descriptors
         return k
+
+    def to_dict(self):
+        """Return a dictionary representation of the domain"""
+        return [variable.to_dict() for variable in self.variables]
+
+    def to_json(self):
+        """Return the a json representation of the domain"""
+        return json.dumps(self.to_dict())
+
+    @staticmethod
+    def from_json(self, json_str):
+        variables_json = json.loads(json_str)
+        variables = []
+        for variable in variables_json:
+            if variable['type'] == "continuous":
+                new_variable = ContinuousVariable.from_json(variable)
+            elif variable['type'] == "discrete":
+                new_variable = DiscreteVariable.from_json(variable)
+            elif variable['type'] == 'descriptors':
+                new_variable =  DescriptorsVariable.from_json(variable)
+            else:
+                raise ValueError(f"Cannot load variable of type:{variable['type']}. Variable should be continuous, discrete or descriptors")
+            variables.append(new_variable)
+        return Domain(variables)
     
     def __add__(self, var):
         return Domain(self._variables + [var])
@@ -375,6 +408,7 @@ class Domain:
 
     def _html_table_rows(self):
         return ''.join(map(lambda l: l._html_table_rows(), self._variables))
+
 
 class DomainError(Exception):
     pass
