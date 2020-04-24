817b2a9042a0ec6662e29c4ffd10b55160d5f3a2
Kobi Felton <kobi.c.f@gmail.com>
2020-04-24 13:55:28 +0100

    Finish draft of code for Snar Benchmark

--- summit/benchmarks/snar.py
+++ summit/benchmarks/snar.py
@@ -1,30 +1,30 @@
 from summit.experiment import Experiment
 from summit.domain import *
+from summit.utils.dataset import DataSet
+import numpy as np
+from scipy.integrate import solve_ivp
 
 class SnarBenchmark(Experiment):
     def __init__(self):
         domain = self._setup_domain()
         super().__init__(domain)
 
-    def _run(self, conditions, **kwargs):
-        pass
-
     def _setup_domain(self):
         domain = Domain()
 
         # Decision variables
         des_1 = "Flowrate of 2,4-difluoronitrobenzene in ethanol (ml/min)"
-        domain += ContinuousVariable(name='flowrate_dfnb',
+        domain += ContinuousVariable(name='q_dfnb',
                                      description=des_1,
                                      bounds=[0, 10])
         
         des_2 = "Flowrate of pyrrolidine in ethanol (mL/min)"
-        domain += ContinuousVariable(name='flowrate_pldn',
+        domain += ContinuousVariable(name='q_pldn',
                                      description=des_2,
                                      bounds=[0, 10])   
 
         des_3 = "Flowrate of ethanol (mL/min)"
-        domain += ContinuousVariable(name='flowrate_eth',
+        domain += ContinuousVariable(name='q_eth',
                                      description=des_3,
                                      bounds=[0, 10])
 
@@ -34,9 +34,8 @@ class SnarBenchmark(Experiment):
                                      bounds=[30, 120])
 
         # Constraints
-        lhs = "flowrate_dfnb+flowrate_pldn+flowrate_eth-10"
+        lhs = "q_dfnb+q_pldn+q_eth-10"
         domain += Constraint(lhs, "<=")
-
 
         # Objectives
         des_5 = 'space time yield'
@@ -54,3 +53,71 @@ class SnarBenchmark(Experiment):
                                      maximize=False)
 
         return domain  
+
+    def _run(self, conditions, **kwargs):
+        T = conditions['temperature']
+        q_dfnb = conditions['q_dfnb']
+        q_pldn = conditions['q_pldn']
+        q_eth = conditions['q_eth']
+        y, e_factor = self._integrate_equations(q_dfnb, q_pldn, q_eth,T)    
+        conditions['sty'] = y
+        conditions['e_factor'] = e_factor
+        return conditions
+
+    def _integrate_equations(self, 
+                             q_dfnb,
+                             q_pldn,
+                             q_eth,
+                             temperature):
+        # Flowrate and residence time
+        V = 5 #mL
+        q_tot = q_dfnb+q_pldn+q_eth
+        tau = V/q_tot
+
+        # Initial Concentrations
+        C_i = np.zeros(5)
+        C_10 = 1
+        C_20 = 2
+        C_i[0] = C_10*q_dfnb/q_tot
+        C_i[1] = C_10*q_pldn/q_tot
+
+        # Integrate
+        res = solve_ivp(self._integrand,[0, tau], C_i,
+                        args=(temperature))
+        C_final = res.y[:, -1]
+
+        # Calculate STY and E-factor
+        sty = C_final[2]/tau
+        rho_eth = 0.789 # g/mL (should adjust to temp, but just using @ 25C)
+        M = [159.09, 71.12, 210.21, 210.21, 261.33] # molecular weights (g/mol)
+        term_2 = [M[i]*C_final[i]*q_tot for i in range(5)
+                 if i!=2]
+        e_factor = (q_eth*rho_eth+term_2)/(M[2]*C_final[2]*q_tot)
+
+        return sty, e_factor
+        
+    def _integrand(self,C, T):
+        # Kinetic Constants
+        R = 8.71
+        T_ref = 90 + 273.71
+        k = lambda k_ref, E_a, T: k_ref*np.exp(-E_a/R*(1/T-1/T_ref))
+        k_a = k(57.9, 33.3, T)
+        k_b = k(2.70, 35.3, T)
+        k_c = k(0.864, 38.9, T)
+        k_d = k(1.63, 44.8, T)
+        
+        # Reaction Rates
+        r = np.zeros(5)
+        r[0] = -(k_a+k_b)*C[0]*C[1]
+        r[1] = -(k_a+k_b)*C[0]*C[1]-k_c*C[1]*C[2]-k_d*C[1]*C[3]
+        r[2] = k_a*C[0]*C[1]-k_c*C[1]*C[2]
+        r[3] = k_a*C[0]*C[1]-k_d*C[1]*C[3]
+        r[4] = k_c*C[1]*C[2]-k_d*C[1]*C[3]
+
+        # Deltas
+        dcdtau = r
+        return dcdtau
+
+
+
+ 
