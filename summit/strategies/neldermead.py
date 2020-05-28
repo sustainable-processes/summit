b65c11fa166f6b91e907d16f1c2ef68fbfca3834
Jan Rittig <63640094+jangerit@users.noreply.github.com>
2020-05-28 08:43:07 +0200

    fix inner loop

--- summit/strategies/neldermead.py
+++ summit/strategies/neldermead.py
@@ -160,7 +160,7 @@ class NelderMead(Strategy):
             param = [param, [invalid_experiments]]
             c_iter += 1
 
-        if c_iter >= 5:
+        if c_iter >= inner_iter_tol:
             raise ValueError("No new points found. Internal stopping criterion is reached.")
 
         # return only valid experiments (invalid experiments are stored in param[1])
