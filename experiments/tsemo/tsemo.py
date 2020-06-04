7a6fc795ec83d2d3a4e0153b2e308be2206dac33
Kobi Felton <kcmf2@cam.ac.uk>
2020-06-04 13:36:39 +0100

    Finish python TSEMO test

--- experiments/tsemo/tsemo.py
+++ experiments/tsemo/tsemo.py
@@ -26,6 +26,6 @@ def dtlz2_test():
 
 
 if __name__ == '__main__':
-    for i in range(20):
+    for i in range(12, 21,1):
         lab = dtlz2_test()
         lab.save(f'data/python/experiment_{i}.csv')
