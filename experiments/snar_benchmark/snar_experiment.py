06038093d6f16e1dbe104fdc0292fb6f33c18aa3
Kobi Felton <kobi.c.f@gmail.com>
2020-06-24 17:12:13 +0100

    Add chimera transform

--- experiments/snar_benchmark/snar_experiment.py
+++ experiments/snar_benchmark/snar_experiment.py
@@ -1,3 +1,5 @@
+import pytest
+
 from summit import *
 from summit.benchmarks import SnarBenchmark
 from summit.strategies import *
@@ -10,16 +12,18 @@ MAX_ITERATIONS=100
 NEPTUNE_PROJECT="sustainable-processes/summit"
 
 
-# strategies = [NelderMead, SNOBFIT, Random, SOBO]
-strategies  = [SOBO]
 experiment = SnarBenchmark()
+transforms = [MultitoSingleObjective(experiment.domain, 
+                                     expression='-sty/1e4+e_factor/100', 
+                                     maximize=False)
+]
 
-for strategy in strategies:
+@pytest.mark.parametrize('strategy', [NelderMead, SNOBFIT, Random, SOBO])
+@pytest.mark.parametrize('transform', transorms)
+@pytest.mark.parametrize('batch_size', [1,5,10,20])
+def test_snar_experiment(strategy, transform, batch_size, num_repeats=20):
     for i in range(NUM_REPEATS):
         experiment.reset()
-        transform = MultitoSingleObjective(experiment.domain, 
-                                           expression='-sty/1e4+e_factor/100', 
-                                           maximize=False)
         s = strategy(experiment.domain, transform=transform)
 
         # Early stopping for local optimization strategies
@@ -29,10 +33,10 @@ for strategy in strategies:
             f_tol = None
 
         r = NeptuneRunner(experiment=experiment, strategy=s, 
-                          neptune_project=NEPTUNE_PROJECT,
-                          neptune_experiment_name=f"snar_experiment_{s.__class__.__name__}_repeat_{i}",
-                          files=["snar_experiment.py"],
-                          max_iterations=MAX_ITERATIONS,
-                          batch_size=1,
-                          f_tol=f_tol)
-        r.run(save_freq=SAVE_FREQ)
+                            neptune_project=NEPTUNE_PROJECT,
+                            neptune_experiment_name=f"snar_experiment_{s.__class__.__name__}_repeat_{i}",
+                            files=["snar_experiment.py"],
+                            max_iterations=MAX_ITERATIONS,
+                            batch_size=1,
+                            f_tol=f_tol)
+        r.run(save_at_end=True)
--- summit/strategies/base.py
+++ summit/strategies/base.py
@@ -134,6 +134,8 @@ def transform_from_dict(d):
         return MultitoSingleObjective.from_dict(d)
     elif d["name"] == "LogSpaceObjectives":
         return LogSpaceObjectives.from_dict(d)
+    elif d["name"] == "Chimera":
+        return Chimera.from_dict(d)
     elif d["name"] == "Transform":
         return Transform.from_dict(d)
 
@@ -278,6 +280,121 @@ class LogSpaceObjectives(Transform):
                 ds[v.name] = np.exp(ds["log_" + v.name])
         return ds
 
+class Chimera(Transform):
+    """ Scalarize a multiobjective problem using Chimera.
+
+    Chimera is a hiearchical multiobjective scalarazation function developed by
+    Häse et al[1]_[2]_. You set the parameter `loss_tolerances` to weight the importance
+    of each objective.
+
+    Parameters
+    ---------- 
+    domain : `sumit.domain.Domain``
+        A domain for that is being used in the strategy
+    loss_tolerances : array-like
+        Tolerances defining the hiearchy of objectives.
+    smoothness : float, optional
+        Smoothing parameter. Defaults to 0.0
+    
+    Examples
+    --------
+
+    Notes
+    ------
+    This code is based on the for Griffyn[2]_.
+    
+    References
+    ----------
+    .. [1] Häse, F., Roch, L. M., & Aspuru-Guzik, A. "Chimera: enabling hierarchy based multi-objective
+           optimization for self-driving laboratories." Chemical Science, 2018, 9,7642-7655
+    .. [2] Häse, F., Roch, L.M. and Aspuru-Guzik, A., 2020. Gryffin: An algorithm for Bayesian 
+           optimization for categorical variables informed by physical intuition with applications to chemistry. 
+           arXiv preprint arXiv:2003.12127.
+    
+    """ 
+    def __init__(self, tolerances, softness = 0.0, absolutes = None):
+        self.tolerances = tolerances
+        self.absolutes  = absolutes
+        if absolutes is None:
+            self.absolutes = np.zeros(len(tolerances)) + np.nan
+        self.softness   = softness
+
+    def soft_step(self, value):
+        arg = - value / self.softness
+        return 1. / (1. + np.exp(arg))
+
+    def hard_step(self, value):
+        result = np.empty(len(value))
+        result = np.where(value > 0., 1., 0.)
+        return result
+
+    def step(self, value):
+        if self.softness < 1e-5:
+            return self.hard_step(value)
+        else:
+            return self.soft_step(value)
+
+    def rescale(self, raw_objs):
+        res_objs = np.empty(raw_objs.shape)
+        res_abs  = np.empty(self.absolutes.shape)
+        for index in range(raw_objs.shape[1]):
+            min_objs, max_objs = np.amin(raw_objs[:, index]), np.amax(raw_objs[:, index])
+            if min_objs < max_objs:
+                res_abs[index]     = (self.absolutes[index] - min_objs) / (max_objs - min_objs)
+                res_objs[:, index] = (raw_objs[:, index] - min_objs) / (max_objs - min_objs)
+            else:
+                res_abs[index]     = self.absolutes[index] - min_objs
+                res_objs[:, index] = raw_objs[:, index] - min_objs
+        return res_objs, res_abs				
+
+    def shift_objectives(self, objs, res_abs):
+        transposed_objs  = objs.transpose()
+        shapes           = transposed_objs.shape
+        shifted_objs     = np.empty((shapes[0] + 1, shapes[1]))
+        
+        mins, maxs, tols = [], [], []
+        domain           = np.arange(shapes[1])
+        shift            = 0
+        for obj_index, obj in enumerate(transposed_objs):
+
+            # get absolute tolerances
+            minimum = np.amin(obj[domain])
+            maximum = np.amax(obj[domain])
+            mins.append(minimum)
+            maxs.append(maximum)
+            tolerance = minimum + self.tolerances[obj_index] * (maximum - minimum)
+            if np.isnan(tolerance):
+                tolerance = res_abs[obj_index]			
+
+            # adjust region of interest
+            interest = np.where(obj[domain] < tolerance)[0]
+            if len(interest) > 0:
+                domain = domain[interest]
+
+            # apply shift	
+            tols.append(tolerance + shift)
+            shifted_objs[obj_index] = transposed_objs[obj_index] + shift
+
+            # compute new shift
+            if obj_index < len(transposed_objs) - 1:
+                shift -= np.amax(transposed_objs[obj_index + 1][domain]) - tolerance
+            else:
+                shift -= np.amax(transposed_objs[0][domain]) - tolerance
+                shifted_objs[obj_index + 1] = transposed_objs[0] + shift
+        return shifted_objs, tols
+
+    def scalarize_objs(self, shifted_objs, abs_tols):
+        scalar_obj = shifted_objs[-1].copy()
+        for index in range(0, len(shifted_objs) - 1)[::-1]:
+            scalar_obj *= self.step( - shifted_objs[index] + abs_tols[index])
+            scalar_obj += self.step(   shifted_objs[index] - abs_tols[index]) * shifted_objs[index]
+        return scalar_obj.transpose()
+
+    def scalarize(self, raw_objs):
+        res_objs, res_abs      = self.rescale(raw_objs)
+        shifted_objs, abs_tols = self.shift_objectives(res_objs, res_abs) 
+        scalarized_obj         = self.scalarize_objs(shifted_objs, abs_tols)
+        return scalarized_obj
 
 class Strategy(ABC):
     """ Base class for strategies 
