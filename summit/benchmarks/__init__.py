6dcd2436f3e8641919763c2f0eb9d60fc5324033
Jan Rittig <63640094+jangerit@users.noreply.github.com>
2020-07-13 20:28:12 +0200

    Experimental emulator (#42)
    ExperimentalEmulator implemented with three examples in ReizmanSuzukiEmulator, BaumgartnerCrossCouplingEmulator, and BaumgartnerCrossCouplingDescriptorEmulator.

--- summit/benchmarks/__init__.py
+++ summit/benchmarks/__init__.py
@@ -1,2 +1,3 @@
 from .snar import SnarBenchmark
 from .test_functions import Himmelblau, Hartmann3D, ThreeHumpCamel
+from .experimental_emulator import ExperimentalEmulator, ReizmanSuzukiEmulator, BaumgartnerCrossCouplingEmulator, BaumgartnerCrossCouplingDescriptorEmulator
