Fix subtle issue with wrong values being passed to spectral sampling

experiments/tsemo/train_gp_matlab_data.py
@@ -59,13 +59,6 @@ def fit_and_test(n_training_matlab, num_restarts=100, max_iters=2000, n_spectral
         print(f"Model {name} variance: {hyp[1]}")
         print(f"Model {name} noise: {hyp[2]}")
 
-    # Spectral sampling
-    if use_spectral_sample:
-        print(f"Spectral sampling with {n_spectral_points} spectral points.")
-        for model in models.models.values():
-            model.spectral_sample(X_train_scaled, y_train_scaled, 
-                                  n_spectral_points=n_spectral_points)
-
     # Model validation
     rmse = lambda pred, actual: np.sqrt(np.mean((pred-actual)**2, axis=0))
 
@@ -82,6 +75,15 @@ def fit_and_test(n_training_matlab, num_restarts=100, max_iters=2000, n_spectral
     y_pred_test = y_pred_test_scaled*y_std+y_mean
     rmse_test= rmse(y_pred_test.to_numpy(), y_test.to_numpy())
     print(f"RMSE test y0 ={rmse_test[0].round(2)}, RMSE test y1={rmse_test[1].round(2)}")
+
+    # Spectral sampling
+    if use_spectral_sample:
+        print(f"Spectral sampling with {n_spectral_points} spectral points.")
+        for name, model in models.models.items():
+            model.spectral_sample(X_train_scaled, y_train_scaled[[name]], 
+                                  n_spectral_points=n_spectral_points)
+
+
 
     # Model validation on spectral sampling
     if  use_spectral_sample:
@@ -132,7 +134,7 @@ def fit_and_test(n_training_matlab, num_restarts=100, max_iters=2000, n_spectral
 if __name__=='__main__':
     results = []
     use_spectral_sample=True
-    n_spectral_points=4000
+    n_spectral_points=1000
     num_restarts=100
     for i in range(10):
         res = fit_and_test(

summit/utils/models.py
@@ -274,16 +274,19 @@ class GPyModel(BaseEstimator, RegressorMixin):
             pass
         else:
             raise TypeError("Y must be a DataSet or numpy array")
+
+        if y.shape[1] > 1:
+            raise ValueError("Y must be 1D")
         
         # Spectral sampling. Clip values to match Matlab implementation
         noise = self._model.Gaussian_noise.variance.values[0]
         sampled_f = None
         for i in range(n_retries):
             try:
-                sampled_f = pyrff.sample_rff(
-                    lengthscales=self._model.kern.lengthscale.values,
-                    scaling=np.sqrt(self._model.kern.variance.values[0]),
-                    noise=np.sqrt(noise),
+                sampled_f = spectral_sample(
+                    lengthscales=self._model.kern.lengthscale.values.round(3),
+                    scaling=np.sqrt(self._model.kern.variance.values[0]).round(3),
+                    noise=noise.round(3),
                     kernel_nu=matern_nu,
                     X=X,
                     Y=y[:,0],
@@ -302,7 +305,7 @@ class GPyModel(BaseEstimator, RegressorMixin):
         def f(x_new):
             y_s = sampled_f(x_new)
             return np.atleast_2d(y_s).T   
-        self.sampled_f = f
+        self.sampled_f = f  
         return self.sampled_f
     
     @property
@@ -342,7 +345,7 @@ def spectral_sample(lengthscales, scaling, noise, kernel_nu, X, Y, M):
         # Get variables from problem structure
         n, D = np.shape(X)
         ell = np.array(lengthscales) 
-        sf2 = scaling
+        sf2 = scaling**2
         sn2 = noise
 
         # Monte carlo samples of W and b
@@ -389,7 +392,7 @@ def spectral_sample(lengthscales, scaling, noise, kernel_nu, X, Y, M):
             inputs, _ = np.shape(x)
             bprime = matlib.repmat(b, 1, inputs)
             output =  (theta.T*np.sqrt(2*sf2/M))@np.cos(W@x.T+bprime)
-            return np.atleast_2d(output).T
+            return output
         return f
 
 class AnalyticalModel(Model):