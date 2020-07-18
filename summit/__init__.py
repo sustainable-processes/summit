7e4ee2a1f5bc47d26f41fbaa04140f58923b680f
Kobi Felton <kobi.c.f@gmail.com>
2020-07-18 10:23:32 +0100

    Create a .summit folder in user home directory and store temporary files there

--- summit/__init__.py
+++ summit/__init__.py
@@ -1,3 +1,11 @@
+import os
+import pathlib
+
+def get_summit_config_path(config_dir_name=".summit"):
+    home = pathlib.Path.home()
+    return home / config_dir_name
+
+
 from summit.domain import *
 from summit.experiment import Experiment
 from summit.run import Runner, NeptuneRunner
