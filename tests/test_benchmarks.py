0e7c535ef9cf71aa852b8a3f5926842674783932
Kobi Felton <kobi.c.f@gmail.com>
2021-01-22 21:37:53 +0000

    Start tests for ANNREgressor

--- tests/test_benchmarks.py
+++ tests/test_benchmarks.py
@@ -1,17 +1,21 @@
 import pytest
 from summit.strategies import Strategy
 from summit.experiment import Experiment
+from summit.domain import *
 from summit.benchmarks import (
     SnarBenchmark,
     DTLZ2,
-    Hartmann3D,
-    Himmelblau,
-    ThreeHumpCamel,
-    ReizmanSuzukiEmulator,
-    BaumgartnerCrossCouplingEmulator,
+    ExperimentalEmulator,
+    ANNRegressor,
+    # Hartmann3D,
+    # Himmelblau,
+    # ThreeHumpCamel,
+    # ReizmanSuzukiEmulator,
+    # BaumgartnerCrossCouplingEmulator,
 )
 from summit.utils.dataset import DataSet
 import numpy as np
+from numpy.random import default_rng
 import os
 
 
@@ -44,6 +48,56 @@ def test_snar_benchmark(noise_level):
     assert b.noise_level == noise_level
 
     return results
+
+
+def create_domain():
+    domain = Domain()
+    domain += ContinuousVariable(
+        name="temperature",
+        description="reaction temperature in celsius",
+        bounds=[30, 100],
+    )
+    domain += ContinuousVariable(
+        name="flowrate_a", description="flowrate of reactant a", bounds=[1, 100]
+    )
+
+    domain += ContinuousVariable(
+        name="flowrate_b", description="flowrate of reactant b", bounds=[1, 100]
+    )
+
+    domain += ContinuousVariable(
+        name="yield",
+        description="yield of reaction",
+        bounds=[0, 100],
+        is_objective=True,
+        maximize=True,
+    )
+    return domain
+
+
+def create_dataset(domain, n_samples=100, random_seed=100):
+    rng = default_rng(random_seed)
+    n_features = len(domain.input_variables)
+    inputs = rng.standard_normal(size=(n_samples, n_features))
+    inputs *= [-5, 6, 0.1]
+    output = np.sum(inputs ** 2, axis=1)
+    data = np.append(inputs, np.atleast_2d(output).T, axis=1)
+    columns = [v.name for v in domain.input_variables] + [
+        domain.output_variables[0].name
+    ]
+    return DataSet(data, columns=columns)
+
+
+def test_experimental_emulator(n_samples=100, max_epochs=100):
+    # Setup
+    domain = create_domain()
+    ds = create_dataset(domain, n_samples=n_samples)
+
+    # Train emulator
+    exp = ExperimentalEmulator("test_model", domain, dataset=ds, regressor=ANNRegressor)
+    exp.train(max_epochs=max_epochs)
+
+    # Evaluate emulator
 
 
 def test_baumgartner_CC_emulator():
@@ -84,4 +138,3 @@ def test_dltz2_benchmark(num_inputs):
     data = b.data
     assert np.isclose(data["y_0"].iloc[0], 0.7071)
     assert np.isclose(data["y_1"].iloc[0], 0.7071)
-
