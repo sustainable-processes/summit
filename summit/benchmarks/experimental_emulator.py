ca51f76ef337fc330c4a11f938e3935dd628d68c
Kobi Felton <kobi.c.f@gmail.com>
2021-02-17 01:12:02 +0000

    Update scikit learn and create progress bar for grid search

--- summit/benchmarks/experimental_emulator.py
+++ summit/benchmarks/experimental_emulator.py
@@ -14,13 +14,36 @@ from blitz.utils import variational_estimator
 from sklearn.compose import ColumnTransformer, TransformedTargetRegressor
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler, OneHotEncoder, FunctionTransformer
-from sklearn.model_selection import train_test_split, cross_validate
+from sklearn.model_selection import (
+    train_test_split,
+    cross_validate,
+    GridSearchCV,
+    ParameterGrid,
+)
+from sklearn.model_selection._search import BaseSearchCV, _check_param_grid
+from sklearn.base import BaseEstimator, is_classifier, clone
+from sklearn.model_selection._split import check_cv
+from sklearn.model_selection._validation import _fit_and_score
 from sklearn.metrics import r2_score
+from sklearn.utils.validation import (
+    _deprecate_positional_args,
+    indexable,
+    check_is_fitted,
+    _check_fit_params,
+)
+from sklearn.utils.fixes import delayed
+from sklearn.metrics._scorer import _check_multimetric_scoring
 
+from tqdm.auto import tqdm
+from joblib import Parallel
 import pathlib
 import numpy as np
 import pandas as pd
 from copy import deepcopy
+from itertools import product
+from collections import defaultdict
+from copy import deepcopy
+import time
 
 
 class ExperimentalEmulator(Experiment):
@@ -110,6 +133,8 @@ class ExperimentalEmulator(Experiment):
             You can pass extra arguments to the regressor here.
         callbacks : None, "disable" or list of Callbacks
             Skorch callbacks passed to skorch.net. See: https://skorch.readthedocs.io/en/latest/net.html
+        verbose : int
+            0 for no logging, 1 for logging
 
         Returns
         -------
@@ -126,17 +151,19 @@ class ExperimentalEmulator(Experiment):
         regressor_kwargs = kwargs.get("regressor_kwargs", {})
         regressor_kwargs.update(
             dict(
-                input_dim=self.caclulate_input_dimensions(),
-                output_dim=len(self.output_variables),
-                n_examples=self.ds.shape[0],
+                module__input_dim=self.caclulate_input_dimensions(),
+                module__output_dim=len(self.output_variables),
+                module__n_examples=self.ds.shape[0],
             )
         )
-        net = EmulatorNet(
+        verbose = kwargs.get("verbose", 0)
+        net = NeuralNetRegressor(
             self.regressor,
-            regressor_kwargs=regressor_kwargs,
             train_split=None,
             max_epochs=kwargs.get("max_epochs", 100),
             callbacks=kwargs.get("callbacks"),
+            verbose=verbose,
+            **regressor_kwargs,
         )
 
         # Create predictor
@@ -149,6 +176,7 @@ class ExperimentalEmulator(Experiment):
                 ("net", net),
             ]
         )
+
         self.predictor = TransformedTargetRegressor(pipe, transformer=StandardScaler())
 
         # Get data
@@ -172,6 +200,16 @@ class ExperimentalEmulator(Experiment):
         # Run training
         scoring = kwargs.get("scoring", ["r2", "neg_root_mean_squared_error"])
         folds = kwargs.get("cv_folds", 5)
+        search_params = kwargs.get("search_params", {})
+        if search_params:
+            self.logger.info("Starting grid search.")
+            gs = ProgressGridSearchCV(
+                self.predictor, search_params, refit="r2", cv=folds, scoring=scoring
+            )
+            gs.fit(self.X_train, y_train)
+            self.predictor.set_params(**gs.best_params_)
+
+        self.logger.info("Starting training via cross validation.")
         res = cross_validate(
             self.predictor,
             self.X_train,
@@ -307,11 +345,15 @@ class ExperimentalEmulator(Experiment):
             # Scores
             handles = []
             r2_train = r2_score(self.y_train[:, i], y_train_pred[:, i])
-            r2_train_patch = mpatches.Patch(label=f"R2 = {r2_train:.2f}", color=color)
+            r2_train_patch = mpatches.Patch(
+                label=f"Train R2 = {r2_train:.2f}", color=color
+            )
             handles.append(r2_train_patch)
             if include_test:
                 r2_test = r2_score(self.y_test[:, i], y_test_pred[:, i])
-                r2_test_patch = mpatches.Patch(label=f"R2 = {r2_test:.2f}", color=color)
+                r2_test_patch = mpatches.Patch(
+                    label=f"Test R2 = {r2_test:.2f}", color=color
+                )
                 handles.append(r2_test_patch)
             axes[i].legend(handles=handles, fontsize=12)
 
@@ -324,31 +366,262 @@ class ExperimentalEmulator(Experiment):
         return fig, axes
 
 
-class EmulatorNet(NeuralNetRegressor):
-    def __init__(self, *args, **kwargs):
-        self.regressor_kwargs = kwargs.pop("regressor_kwargs", {})
-        super().__init__(*args, **kwargs)
-
-    def initialize_module(self):
-        kwargs = self.regressor_kwargs
-        module = self.module
-        is_initialized = isinstance(module, torch.nn.Module)
-        if kwargs or not is_initialized:
-            if is_initialized:
-                module = type(module)
-
-            if (is_initialized or self.initialized_) and self.verbose:
-                msg = self._format_reinit_msg("module", kwargs)
-                print(msg)
-
-            module = module(**kwargs)
-        self.module_ = to_device(module, self.device)
-        return self
-
-
 def numpy_to_tensor(X):
     """Convert datasets into """
     return torch.tensor(X).float()
+
+
+class ProgressParallel(Parallel):
+    def __init__(self, use_tqdm=True, total=None, *args, **kwargs):
+        self._use_tqdm = use_tqdm
+        self._total = total
+        super().__init__(*args, **kwargs)
+
+    def __call__(self, *args, **kwargs):
+        with tqdm(disable=not self._use_tqdm, total=self._total) as self._pbar:
+            return Parallel.__call__(self, *args, **kwargs)
+
+    @property
+    def total(self):
+        return self._total
+
+    @total.setter
+    def total(self, val):
+        self._total = val
+
+    def print_progress(self):
+        if self._total is None:
+            self._pbar.total = self.n_dispatched_tasks
+        self._pbar.n = self.n_completed_tasks
+        self._pbar.refresh()
+
+
+class ProgressGridSearchCV(BaseSearchCV):
+    @_deprecate_positional_args
+    def __init__(
+        self,
+        estimator,
+        param_grid,
+        *,
+        scoring=None,
+        n_jobs=None,
+        refit=True,
+        cv=None,
+        verbose=0,
+        pre_dispatch="2*n_jobs",
+        error_score=np.nan,
+        return_train_score=False,
+    ):
+        super().__init__(
+            estimator=estimator,
+            scoring=scoring,
+            n_jobs=n_jobs,
+            refit=refit,
+            cv=cv,
+            verbose=verbose,
+            pre_dispatch=pre_dispatch,
+            error_score=error_score,
+            return_train_score=return_train_score,
+        )
+        self.param_grid = param_grid
+        _check_param_grid(param_grid)
+
+    def _run_search(self, evaluate_candidates):
+        """Search all candidates in param_grid"""
+        evaluate_candidates(ParameterGrid(self.param_grid))
+
+    @_deprecate_positional_args
+    def fit(self, X, y=None, *, groups=None, **fit_params):
+        """Run fit with all sets of parameters.
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            Training vector, where n_samples is the number of samples and
+            n_features is the number of features.
+        y : array-like of shape (n_samples, n_output) \
+            or (n_samples,), default=None
+            Target relative to X for classification or regression;
+            None for unsupervised learning.
+        groups : array-like of shape (n_samples,), default=None
+            Group labels for the samples used while splitting the dataset into
+            train/test set. Only used in conjunction with a "Group" :term:`cv`
+            instance (e.g., :class:`~sklearn.model_selection.GroupKFold`).
+        **fit_params : dict of str -> object
+            Parameters passed to the ``fit`` method of the estimator
+        """
+        estimator = self.estimator
+        refit_metric = "score"
+
+        if callable(self.scoring):
+            scorers = self.scoring
+        elif self.scoring is None or isinstance(self.scoring, str):
+            scorers = check_scoring(self.estimator, self.scoring)
+        else:
+            scorers = _check_multimetric_scoring(self.estimator, self.scoring)
+            # self._check_refit_for_multimetric(scorers)
+            refit_metric = self.refit
+
+        X, y, groups = indexable(X, y, groups)
+        fit_params = _check_fit_params(X, fit_params)
+
+        cv_orig = check_cv(self.cv, y, classifier=is_classifier(estimator))
+        n_splits = cv_orig.get_n_splits(X, y, groups)
+
+        base_estimator = clone(self.estimator)
+
+        parallel = ProgressParallel(n_jobs=self.n_jobs, pre_dispatch=self.pre_dispatch)
+
+        fit_and_score_kwargs = dict(
+            scorer=scorers,
+            fit_params=fit_params,
+            return_train_score=self.return_train_score,
+            return_n_test_samples=True,
+            return_times=True,
+            return_parameters=False,
+            error_score=self.error_score,
+            verbose=self.verbose,
+        )
+        results = {}
+        with parallel:
+            all_candidate_params = []
+            all_out = []
+            all_more_results = defaultdict(list)
+
+            def evaluate_candidates(candidate_params, cv=None, more_results=None):
+                cv = cv or cv_orig
+                candidate_params = list(candidate_params)
+                n_candidates = len(candidate_params)
+
+                if self.verbose > 0:
+                    print(
+                        "Fitting {0} folds for each of {1} candidates,"
+                        " totalling {2} fits".format(
+                            n_splits, n_candidates, n_candidates * n_splits
+                        )
+                    )
+                runs = product(
+                    enumerate(candidate_params), enumerate(cv.split(X, y, groups))
+                )
+                parallel.total = len(list(deepcopy(runs)))
+                out = parallel(
+                    delayed(_fit_and_score)(
+                        clone(base_estimator),
+                        X,
+                        y,
+                        train=train,
+                        test=test,
+                        parameters=parameters,
+                        split_progress=(split_idx, n_splits),
+                        candidate_progress=(cand_idx, n_candidates),
+                        **fit_and_score_kwargs,
+                    )
+                    for (cand_idx, parameters), (split_idx, (train, test)) in runs
+                )
+
+                if len(out) < 1:
+                    raise ValueError(
+                        "No fits were performed. "
+                        "Was the CV iterator empty? "
+                        "Were there no candidates?"
+                    )
+                elif len(out) != n_candidates * n_splits:
+                    raise ValueError(
+                        "cv.split and cv.get_n_splits returned "
+                        "inconsistent results. Expected {} "
+                        "splits, got {}".format(n_splits, len(out) // n_candidates)
+                    )
+
+                # For callable self.scoring, the return type is only know after
+                # calling. If the return type is a dictionary, the error scores
+                # can now be inserted with the correct key. The type checking
+                # of out will be done in `_insert_error_scores`.
+                if callable(self.scoring):
+                    _insert_error_scores(out, self.error_score)
+                all_candidate_params.extend(candidate_params)
+                all_out.extend(out)
+                if more_results is not None:
+                    for key, value in more_results.items():
+                        all_more_results[key].extend(value)
+
+                nonlocal results
+                results = self._format_results(
+                    all_candidate_params, n_splits, all_out, all_more_results
+                )
+
+                return results
+
+            self._run_search(evaluate_candidates)
+
+            # multimetric is determined here because in the case of a callable
+            # self.scoring the return type is only known after calling
+            first_test_score = all_out[0]["test_scores"]
+            self.multimetric_ = isinstance(first_test_score, dict)
+
+            # check refit_metric now for a callabe scorer that is multimetric
+            if callable(self.scoring) and self.multimetric_:
+                self._check_refit_for_multimetric(first_test_score)
+                refit_metric = self.refit
+
+        # For multi-metric evaluation, store the best_index_, best_params_ and
+        # best_score_ iff refit is one of the scorer names
+        # In single metric evaluation, refit_metric is "score"
+        if self.refit or not self.multimetric_:
+            # If callable, refit is expected to return the index of the best
+            # parameter set.
+            if callable(self.refit):
+                self.best_index_ = self.refit(results)
+                if not isinstance(self.best_index_, numbers.Integral):
+                    raise TypeError("best_index_ returned is not an integer")
+                if self.best_index_ < 0 or self.best_index_ >= len(results["params"]):
+                    raise IndexError("best_index_ index out of range")
+            else:
+                self.best_index_ = results["rank_test_%s" % refit_metric].argmin()
+                self.best_score_ = results["mean_test_%s" % refit_metric][
+                    self.best_index_
+                ]
+            self.best_params_ = results["params"][self.best_index_]
+
+        if self.refit:
+            # we clone again after setting params in case some
+            # of the params are estimators as well.
+            self.best_estimator_ = clone(
+                clone(base_estimator).set_params(**self.best_params_)
+            )
+            refit_start_time = time.time()
+            if y is not None:
+                self.best_estimator_.fit(X, y, **fit_params)
+            else:
+                self.best_estimator_.fit(X, **fit_params)
+            refit_end_time = time.time()
+            self.refit_time_ = refit_end_time - refit_start_time
+
+        # Store the only scorer not as a dict for single metric evaluation
+        self.scorer_ = scorers
+
+        self.cv_results_ = results
+        self.n_splits_ = n_splits
+
+        return self
+
+    def _check_refit_for_multimetric(self, scores):
+        """Check `refit` is compatible with `scores` is valid"""
+        multimetric_refit_msg = (
+            "For multi-metric scoring, the parameter refit must be set to a "
+            "scorer key or a callable to refit an estimator with the best "
+            "parameter setting on the whole data and make the best_* "
+            "attributes available for that metric. If this is not needed, "
+            f"refit should be set to False explicitly. {self.refit!r} was "
+            "passed."
+        )
+
+        valid_refit_dict = isinstance(self.refit, str) and self.refit in scores
+
+        if (
+            self.refit is not False
+            and not valid_refit_dict
+            and not callable(self.refit)
+        ):
+            raise ValueError(multimetric_refit_msg)
 
 
 @variational_estimator
@@ -478,7 +751,7 @@ class ANNRegressor(torch.nn.Module):
         self.input_layer = torch.nn.Linear(input_dim, hidden_units)
         if num_hidden_layers > 0:
             self.hidden_layers = torch.nn.Sequential(
-                [torch.nn.linear(hidden_units, hidden_units)]
+                *[torch.nn.Linear(hidden_units, hidden_units)]
             )
         self.output_layer = torch.nn.Linear(hidden_units, output_dim)
         self.criterion = torch.nn.MSELoss()
