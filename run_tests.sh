605044364808db056bb1a2e90c74bebb29f7a262
Kobi Felton <kobi.c.f@gmail.com>
2022-04-25 00:04:52 +0100

    Use console script to run tests

--- run_tests.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-SCRIPT_PATH="${BASH_SOURCE}"
-while [ -L "${SCRIPT_PATH}" ]; do
-    SCRIPT_DIR="$(cd -P "$(dirname "${SCRIPT_PATH}")" >/dev/null 2>&1 && pwd)"
-    SCRIPT_PATH="$(readlink "${SCRIPT_PATH}")"
-    [[ ${SCRIPT_PATH} != /* ]] && SCRIPT_PATH="${SCRIPT_DIR}/${SCRIPT_PATH}"
-done
-SCRIPT_DIR="$(cd -P "$(dirname -- "${SCRIPT_PATH}")" >/dev/null 2>&1 && pwd)"
-pytest $SCRIPT_DIR --ignore=experiments --doctest-modules --disable-warnings
