91106d561654cbbddba4a82be2b0850b1ae4cbea
Kobi Felton <kobi.c.f@gmail.com>
2022-04-24 23:02:15 +0100

    Correct script path

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,9 @@
 #/bin/bash
-# package_path=$(python -c "import sys;print(sys.prefix)")
-# summit_path="$package_path/site-packages/summit"
-# summit_path=$(python -c "import importlib.util;print(importlib.util.find_spec('summit').submodule_search_locations[0])")
-# pytest $summit_path $summit_path/../tests --doctest-modules --disable-warnings
+SCRIPT_PATH="${BASH_SOURCE}"
+while [ -L "${SCRIPT_PATH}" ]; do
+    SCRIPT_DIR="$(cd -P "$(dirname "${SCRIPT_PATH}")" >/dev/null 2>&1 && pwd)"
+    SCRIPT_PATH="$(readlink "${SCRIPT_PATH}")"
+    [[ ${SCRIPT_PATH} != /* ]] && SCRIPT_PATH="${SCRIPT_DIR}/${SCRIPT_PATH}"
+done
+SCRIPT_DIR="$(cd -P "$(dirname -- "${SCRIPT_PATH}")" >/dev/null 2>&1 && pwd)"
+pytest $SCRIPT_DIR --ignore=experiments --doctest-modules --disable-warnings
